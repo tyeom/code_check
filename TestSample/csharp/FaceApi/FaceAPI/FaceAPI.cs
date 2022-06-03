28b9c2d2f8bc8b39424c6f20da754bf60b639eac
tyeom <r.Until@gmail.com>
2022-06-03 12:15:22 +0900

    FaceApi Sample code

---
+++ TestSample/csharp/FaceApi/FaceAPI/FaceAPI.cs
@@ -0,0 +1,107 @@
+﻿namespace FaceAPI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Microsoft.Azure.CognitiveServices.Vision.Face;
+    using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+
+    public class FaceAPI
+    {
+        private const string _SUBSCRIPTION_KEY_ = "8e2b634b7c804a7?????????";
+        //private const string _FACE_ENDPOINT_ = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0";
+        private const string _FACE_ENDPOINT_ = "https://westcentralus.api.cognitive.microsoft.com";
+        private readonly FaceAttributeType[] _faceAttributes = { FaceAttributeType.Age, FaceAttributeType.Gender };
+        private FaceClient _faceClient;
+
+        public delegate void GetFaceAttributesCallback(double? age, Gender? gender);
+
+        public enum Gender
+        {
+            Male = 0,
+            Female = 1,
+            Genderless = 2
+        }
+
+        public void Init()
+        {
+            if (_faceClient != null)
+            {
+                _faceClient.Dispose();
+                _faceClient = null;
+            }
+
+            _faceClient = new FaceClient(
+                new ApiKeyServiceClientCredentials(_SUBSCRIPTION_KEY_),
+                new System.Net.Http.DelegatingHandler[] { });
+            _faceClient.Endpoint = _FACE_ENDPOINT_;
+        }
+
+        public async Task GetFaceAttributesForLocal(string imagePath, GetFaceAttributesCallback callback)
+        {
+            if (!File.Exists(imagePath))
+                throw new FileNotFoundException();
+
+            try
+            {
+                using (Stream imageStream = File.OpenRead(imagePath))
+                {
+                    IList<DetectedFace> faceList =
+                            await _faceClient.Face.DetectWithStreamAsync(
+                                imageStream, true, false, _faceAttributes);
+
+                    if (faceList == null)
+                    {
+                        throw new Exception("faceList is null");
+                    }
+                    else if (faceList.Count <= 0)
+                    {
+                        throw new Exception("No recognized face.");
+                    }
+                    else
+                    {
+                        callback(faceList[0].FaceAttributes.Age, (Gender)faceList[0].FaceAttributes.Gender);
+                    }
+                }
+            }
+            catch (APIErrorException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task GetFaceAttributesForUrl(string imageUrl, GetFaceAttributesCallback callback)
+        {
+            if (!Uri.IsWellFormedUriString(imageUrl, UriKind.Absolute))
+                throw new UriFormatException();
+
+            try
+            {
+                IList<DetectedFace> faceList =
+                            await _faceClient.Face.DetectWithUrlAsync(
+                                imageUrl, true, false, _faceAttributes);
+
+                if (faceList == null)
+                {
+                    throw new Exception("faceList is null");
+                }
+                else if (faceList.Count <= 0)
+                {
+                    throw new Exception("No recognized face.");
+                }
+                else
+                {
+                    callback(faceList[0].FaceAttributes.Age, (Gender)faceList[0].FaceAttributes.Gender);
+                }
+            }
+            catch (APIErrorException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ TestSample/csharp/FaceApi/FaceApiClient/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FaceApiClient
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private FaceAPI.FaceAPI _faceAPI;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _faceAPI = new FaceAPI.FaceAPI();
+            _faceAPI.Init();
+        }
+
+        private void Local_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _faceAPI.GetFaceAttributesForLocal(openFileDialog.FileName, this.GetFaceAttributesCallback);
+
+                this.xImg.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+            }
+        }
+
+        private void Remote_Click(object sender, RoutedEventArgs e)
+        {
+            string imageUrl = "https://ssl.pstatic.net/mimgnews/image/079/2011/02/22/22155602009000_61000090.jpg";
+            _faceAPI.GetFaceAttributesForUrl(imageUrl, this.GetFaceAttributesCallback);
+
+            this.xImg.Source = new BitmapImage(new Uri(imageUrl));
+        }
+
+        private void GetFaceAttributesCallback(double? age, FaceAPI.FaceAPI.Gender? gender)
+        {
+            string result = string.Format("Age : {0} / Gender : {1}", age.ToString(), gender.ToString());
+            MessageBox.Show(result);
+        }
+    }
+}
