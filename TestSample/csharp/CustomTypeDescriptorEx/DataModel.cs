2e999cd32ad40560bfa914ac582604ad9dd87737
tyeom <r.Until@gmail.com>
2022-12-27 16:02:24 +0900

    Add files via upload

---
+++ TestSample/csharp/CustomTypeDescriptorEx/DataModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICustomTypeDescriptorEx;
+
+public class DataModel : ModelBase
+{
+    //public string? UserID
+    //{
+    //    get;set;
+    //}
+
+    //public string? UserName
+    //{
+    //    get; set;
+    //}
+
+    //public bool UserChecked
+    //{
+    //    get; set;
+    //}
+}
