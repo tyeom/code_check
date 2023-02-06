ecc1e2613631164e97b7714707a551b70d75cbae
tyeom <r.Until@gmail.com>
2023-02-06 16:51:56 +0900

    Add files via upload

---
+++ TestSample/csharp/Method_Interception/Intercepting_Method_RealProxy/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intercepting_Method_RealProxy
+{
+    internal class Program
+    {
+        private int Add(int a, int b) => a + b;
+        public int Val { get; set; }
+
+        static void Main(string[] args)
+        {
+            var test = new TestClass();
+            var proxy = RuntimeProxy.Create<TestClass>(test,
+                // 실제 클래스 메서드 호출 전 가로채기 처리 Func
+                t =>
+                {
+                    try
+                    {
+                        Console.WriteLine("Executing...!");
+                        return t.Invoke();
+                    }
+                    finally
+                    {
+                        Console.WriteLine("Executed!");
+                    }
+                });
+
+            var res = proxy.Add(3, 4);
+            Console.WriteLine(res);
+            proxy.Val= 5;
+            Console.WriteLine(proxy.Val);
+
+            Console.ReadLine();
+        }
+    }
+
+    class TestClass : MarshalByRefObject
+    {
+        public int Add(int a, int b) => a + b;
+        public int Val { get; set; }
+    }
+}
---
+++ TestSample/csharp/Method_Interception/Intercepting_Method_RealProxy/RuntimeProxyInvoker.cs
@@ -0,0 +1,41 @@
+﻿namespace Intercepting_Method_RealProxy
+{
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Reflection;
+
+    public abstract class RuntimeProxyInvoker
+    {
+        public readonly object Target;
+        public readonly MethodInfo Method;
+        public readonly ReadOnlyCollection<object> Arguments;
+
+        public RuntimeProxyInvoker(object target, MethodInfo method, object[] args)
+        {
+            this.Target = target;
+            this.Method = method;
+            this.Arguments = new ReadOnlyCollection<object>(args);
+        }
+
+        public object Invoke()
+        {
+            return Invoke(this.Target);
+        }
+
+        public object Invoke(object target)
+        {
+            if (target == null)
+                throw new ArgumentNullException("target");
+
+            try
+            {
+                return this.Method.Invoke(target, this.Arguments.ToArray());
+            }
+            catch (TargetInvocationException ex)
+            {
+                throw ex.InnerException;
+            }
+        }
+    }
+}
