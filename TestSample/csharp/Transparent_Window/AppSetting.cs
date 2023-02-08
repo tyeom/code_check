e853516084dc4bfa72db8c8a312560bfe3a6ded3
tyeom <r.Until@gmail.com>
2023-02-08 13:27:46 +0900

    Add files via upload

---
+++ TestSample/csharp/Transparent_Window/AppSetting.cs
@@ -0,0 +1,46 @@
+﻿namespace Transparent_Window
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+
+    public class AppSetting : NotifyPropertyChanged
+    {
+        private static readonly Lazy<AppSetting> lazy =
+        new Lazy<AppSetting>(() => new AppSetting());
+        /// <summary>
+        /// 항상 위에 보이기 여부
+        /// </summary>
+        private bool _isTopMose = true;
+
+        public static AppSetting Instance { get { return lazy.Value; } }
+
+        /// <summary>
+        /// 항상 위에 보이기 여부
+        /// </summary>
+        public bool IsTopMost
+        {
+            get => _isTopMose;
+            set
+            {
+                if (_isTopMose != value)
+                {
+                    _isTopMose = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 마우스 이벤트 허용 여부
+        /// </summary>
+        public bool IsMouseEventMessagePass
+        {
+            get;set;
+        }
+    }
+}
