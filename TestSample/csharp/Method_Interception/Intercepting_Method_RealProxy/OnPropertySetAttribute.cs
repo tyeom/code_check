f51a767edf55dd808a38496388d27664183ee1bc
tyeom <r.Until@gmail.com>
2023-02-08 09:45:53 +0900

    Add files via upload

---
+++ TestSample/csharp/Method_Interception/Intercepting_Method_RealProxy/OnPropertySetAttribute.cs
@@ -0,0 +1,31 @@
+﻿namespace Intercepting_Method_Cauldron.Interception
+{
+    using Cauldron.Interception;
+    using System;
+
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public sealed class OnPropertySetAttribute : Attribute, IPropertySetterInterceptor
+    {
+        [AssignMethod("{CtorArgument:0}")]
+        public Action<string, object> _onSetMethod = null;
+
+        public OnPropertySetAttribute(string methodName)
+        {
+            
+        }
+
+        public void OnException(Exception e)
+        {
+        }
+
+        public void OnExit()
+        {
+        }
+
+        public bool OnSet(PropertyInterceptionInfo propertyInterceptionInfo, object oldValue, object newValue)
+        {
+            this._onSetMethod?.Invoke(propertyInterceptionInfo.PropertyName, newValue);
+            return false;
+        }
+    }
+}
--- TestSample/csharp/Method_Interception/Intercepting_Method_RealProxy/Program.cs
+++ TestSample/csharp/Method_Interception/Intercepting_Method_RealProxy/Program.cs
@@ -1,47 +1,34 @@
 ﻿using System;
-using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Intercepting_Method_RealProxy
+namespace Intercepting_Method_Cauldron.Interception
 {
     internal class Program
     {
-        private int Add(int a, int b) => a + b;
-        public int Val { get; set; }
-
         static void Main(string[] args)
         {
             var test = new TestClass();
-            var proxy = RuntimeProxy.Create<TestClass>(test,
-                // 실제 클래스 메서드 호출 전 가로채기 처리 Func
-                t =>
-                {
-                    try
-                    {
-                        Console.WriteLine("Executing...!");
-                        return t.Invoke();
-                    }
-                    finally
-                    {
-                        Console.WriteLine("Executed!");
-                    }
-                });
-
-            var res = proxy.Add(3, 4);
+            var res = test.Add(3, 4);
             Console.WriteLine(res);
-            proxy.Val= 5;
-            Console.WriteLine(proxy.Val);
+            test.Val = 5;
+            Console.WriteLine(test.Val);
 
             Console.ReadLine();
         }
     }
 
-    class TestClass : MarshalByRefObject
+
+    [Logger]
+    [OnPropertySet(nameof(ValueChangelog))]
+    class TestClass
     {
         public int Add(int a, int b) => a + b;
         public int Val { get; set; }
+
+        private void ValueChangelog(string propertyName, object newValue) =>
+            Console.WriteLine($"Name : '{propertyName}' / Value : {newValue}");
     }
 }
